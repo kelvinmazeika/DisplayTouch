f2fb1a59cbbc1930cf783583ad0d1e090d5f913e
KELVIN TAGLIAFERRO MAZEIKA <51738204898@info.local>
2026-05-29 17:09:28 -0300

    Foi corrigido os erros do Nextion

--- include/NextionManager.h
+++ include/NextionManager.h
@@ -9,6 +9,21 @@ void configurarTelaInicial();
 void configurarEventosNextion();
 
 void botaoBack();
+void telaLuz();
+void telaAr();
+void telaProj();
+void telaTv();
+void telaSens();
+void telaSettings();
+
+void addSalaExtraLuz();
+void addSalaExtraAc();
+void addSalaExtraProj();
+void addSalaExtraTv();
+void removeSalaExtraLuz();
+void removeSalaExtraAc();
+void removeSalaExtraProj();
+void removeSalaExtraTv();
 
 
 #endif
