2d599b35fd6f17be3fa0c0e26e7219b57fddc20e
Kelvin Mazeika <kelvin.mzk@gmail.com>
2026-05-28 22:10:55 -0300

    Adicionado os botões do Display no DisplayButtons.cpp

---
+++ include/DisplayButtons.h
@@ -0,0 +1,175 @@
+#ifndef DISPLAYBUTTONS_H
+#define DISPLAYBUTTONS_H
+#include <Nextion.h>
+#include <Arduino.h>
+
+extern NexButton botaoBackLuz1;
+extern NexButton botaoBackLuz2;
+extern NexButton botaoBackAc1;
+extern NexButton botaoBackAc2;
+extern NexButton botaoBackAc3;
+extern NexButton botaoBackProj1;
+extern NexButton botaoBackProj2;
+extern NexButton botaoBackTv1;
+extern NexButton botaoBackTv2;
+extern NexButton botaoBackTv3;
+
+extern NexButton botaoLuz;
+extern NexButton botaoAr;
+extern NexButton botaoProj;
+extern NexButton botaoTv;
+extern NexButton botaoSens;
+extern NexButton botaoSettings;
+
+extern NexButton botaoLuzA;
+extern NexButton botaoLuzB;
+extern NexButton acenderTodasLuzes1;
+extern NexButton apagarTodasLuzes1;
+extern NexButton configurarTimerLuzes;
+extern NexButton adicionarSalaExtraLuz;
+
+extern NexButton botaoLuzA2;
+extern NexButton botaoLuzB2;
+extern NexButton botaoLuzC2;
+extern NexButton botaoLuzD2;
+extern NexButton acenderTodasLuzes2;
+extern NexButton apagarTodasLuzes2;
+extern NexButton configurarTimerLuzes2;
+extern NexButton removerSalaExtraLuz;
+
+// Tela Ar Condicionado (3, 4, 5)
+
+extern NexButton botaoAcA;
+extern  NexButton aumentarTempAc;
+extern NexButton diminuirTempAc;
+extern NexButton modoCoolAc;
+extern NexButton modoFanAc;
+extern NexButton modoDryAc;
+extern NexButton modoHeatAc;
+extern NexButton ventoAutoAc;
+extern NexButton ventoBaixoAc;
+extern NexButton ventoMedioAc;
+extern NexButton ventoAltoAc;
+extern NexButton botaoAcB;
+extern NexButton aumentarTempAcB;
+extern NexButton diminuirTempAcB;
+extern NexButton modoCoolAcB;
+extern NexButton modoFanAcB;
+extern NexButton modoDryAcB;
+extern NexButton modoHeatAcB;
+extern NexButton ventoAutoAcB;
+extern NexButton ventoBaixoAcB;
+extern NexButton ventoMedioAcB;
+extern NexButton ventoAltoAcB;
+extern NexButton adicionarSalaExtraAc;
+
+extern NexButton botaoAcA2;
+extern NexButton aumentarTempAc2;
+extern NexButton diminuirTempAc2;
+extern NexButton modoCoolAc2;
+extern NexButton modoFanAc2;
+extern NexButton modoDryAc2;
+extern NexButton modoHeatAc2;
+extern NexButton ventoAutoAc2;
+extern NexButton ventoBaixoAc2;
+extern NexButton ventoMedioAc2;
+extern NexButton ventoAltoAc2;
+extern NexButton botaoAcB2;
+extern NexButton aumentarTempAcB2;
+extern NexButton diminuirTempAcB2;
+extern NexButton modoCoolAcB2;
+extern NexButton modoFanAcB2;
+extern NexButton modoDryAcB2;
+extern NexButton modoHeatAcB2;
+extern NexButton ventoAutoAcB2;
+extern NexButton ventoBaixoAcB2;
+extern NexButton ventoMedioAcB2;
+extern NexButton ventoAltoAcB2;
+extern NexButton removerSalaExtraAc;
+extern NexButton pageAc2;
+
+extern NexButton botaoAcC3;
+extern NexButton aumentarTempAc3;
+extern NexButton diminuirTempAc3;
+extern NexButton modoCoolAc3;
+extern NexButton modoFanAc3;
+extern NexButton modoDryAc3;
+extern NexButton modoHeatAc3;
+extern NexButton ventoAutoAc3;
+extern NexButton ventoBaixoAc3;
+extern NexButton ventoMedioAc3;
+extern NexButton ventoAltoAc3;
+extern NexButton removerSalaExtraAc3;
+extern NexButton botaoAcD3;
+extern NexButton aumentarTempAcD3;
+extern NexButton diminuirTempAcD3;
+extern NexButton modoCoolAcD3;
+extern NexButton modoFanAcD3;
+extern NexButton modoDryAcD3;
+extern NexButton modoHeatAcD3;
+extern NexButton ventoAutoAcD3;
+extern NexButton ventoBaixoAcD3;
+extern NexButton ventoMedioAcD3;
+extern NexButton ventoAltoAcD3;
+extern NexButton pageAc3;
+
+// Tela Projetor (6, 7)
+
+extern NexButton adicionarSalaExtraProj;
+extern NexButton telaRetratilUp;
+extern NexButton telaRetratilStop;
+extern NexButton telaRetratilDown;
+extern NexButton projetorAOn;
+extern NexButton projetorAFreeze;
+
+extern NexButton removerSalaExtraProj;
+extern NexButton telaRetratilUp2;
+extern NexButton telaRetratilStop2;
+extern NexButton telaRetratilDown2;
+extern NexButton projetorAOn2;
+extern NexButton projetorAFreeze2;
+extern NexButton telaRetratilBUp2;
+extern NexButton telaRetratilBStop2;
+extern NexButton telaRetratilBDown2;
+extern NexButton projetorBOn2;
+extern NexButton projetorBFreeze2;
+
+// Tela TV (8, 9, 10)
+
+extern NexButton adicionarSalaExtraTv;
+extern NexButton tvAOn;
+extern NexButton tvAReturn;
+extern NexButton tvAVolumeUp;
+extern NexButton tvAVolumeDown;
+extern NexButton tvAUp;
+extern NexButton tvADown;
+extern NexButton tvALeft;
+extern NexButton tvARight;
+extern NexButton tvASelect;
+
+extern NexButton removerSalaExtraTv;
+extern NexButton tvAOn2;
+extern NexButton tvAReturn2;
+extern NexButton tvAVolumeUp2;
+extern NexButton tvAVolumeDown2;
+extern NexButton tvAUp2;
+extern NexButton tvADown2;
+extern NexButton tvALeft2;
+extern NexButton tvARight2;
+extern NexButton tvASelect2;
+extern NexButton tvPageB2;
+
+extern NexButton removerSalaExtraTv2;
+extern NexButton tvBOn3;
+extern NexButton tvBReturn3;
+extern NexButton tvBVolumeUp3;
+extern NexButton tvBVolumeDown3;
+extern NexButton tvBUp3;
+extern NexButton tvBDown3;
+extern NexButton tvBLeft3;
+extern NexButton tvBRight3;
+extern NexButton tvBSelect3;
+extern NexButton tvPageA3;
+
+
+#endif
--- src/NextionManager.cpp
+++ src/NextionManager.cpp
@@ -1,11 +1,16 @@
 #include <Arduino.h>
 #include <Nextion.h>
+#include "NextionManager.h"
+#include "DisplayButtons.h"
 
 // =========================================================
 // CONFIGURAÇÃO DA COMUNICAÇÃO COM O NEXTION
 // =========================================================
 
 bool luzSalaExtra = 0;
+bool arSalaExtra = 0;
+bool projSalaExtra = 0;
+bool tvSalaExtra = 0;
 
 // Velocidade padrão de comunicação serial do Nextion
 const uint32_t BAUD_NEXTION = 9600;
@@ -15,32 +20,6 @@ const int8_t PINO_RX_NEXTION = 18;
 
 // TX do ESP32-S3 ligado ao RX do Nextion
 const int8_t PINO_TX_NEXTION = 17;
-
-// =========================================================
-// COMPONENTES DO NEXTION
-// =========================================================
-
-// A estrutura é:
-// Classe nomeDoObjeto(pageId, componentId, "nomeNoNextion");
-
-NexButton botaoBackLuz1(1, 4, "b0");
-NexButton botaoBackLuz2(2, 8, "b0");
-NexButton botaoBackAc1(3, 2, "b0");
-NexButton botaoBackAc2(5, 2, "b0");
-NexButton botaoBackAc3(5, 2, "b0");
-NexButton botaoBackProj1(6, 2, "b0");
-NexButton botaoBackProj2(7, 2, "b0");
-NexButton botaoBackTv1(8, 2, "b0");
-NexButton botaoBackTv2(9, 2, "b0");
-NexButton botaoBackTv3(10, 2, "b0");
-
-// Tela Inicial
-
-NexButton botaoLuz(0, 2, "b0");
-
-// =========================================================
-// VARIÁVEIS DE CONTROLE DA APLICAÇÃO
-// =========================================================
 
 // Buffer usado para montar textos antes de enviar ao display
 char texto[50];
@@ -54,10 +33,22 @@ void configurarTelaInicial();
 void configurarEventosNextion();
 
 void botaoBack();
+void telaLuz();
+void telaAr();
+void telaProj();
+void telaTv();
+void telaSens();
+void telaSettings();
 
-// =========================================================
-// CONFIGURAÇÃO DO NEXTION
-// =========================================================
+void addSalaExtraLuz();
+void addSalaExtraAc();
+void addSalaExtraProj();
+void addSalaExtraTv();
+void removeSalaExtraLuz();
+void removeSalaExtraAc();
+void removeSalaExtraProj();
+void removeSalaExtraTv();
+
 
 void configurarNextion()
 {
@@ -78,10 +69,6 @@ void configurarNextion()
     }
 }
 
-// =========================================================
-// CONFIGURAÇÃO INICIAL DA TELA
-// =========================================================
-
 void configurarTelaInicial()
 {
     // Garante que o display esteja na página principal.
@@ -91,15 +78,8 @@ void configurarTelaInicial()
     delay(500);
 }
 
-// =========================================================
-// CONFIGURAÇÃO DOS EVENTOS DO NEXTION
-// =========================================================
-
 void configurarEventosNextion()
 {
-    // attachPop() executa a função quando o componente é solto.
-    // No Nextion Editor, use "Send Component ID" em Touch Release Event.
-
     botaoBackLuz1.attachPop(botaoBack);
     botaoBackLuz2.attachPop(botaoBack);
     botaoBackAc1.attachPop(botaoBack);
@@ -112,7 +92,24 @@ void configurarEventosNextion()
     botaoBackTv3.attachPop(botaoBack);
     
     botaoLuz.attachPop(telaLuz);
-    
+    botaoAr.attachPop(telaAr);
+    botaoProj.attachPop(telaProj);
+    botaoTv.attachPop(telaTv);
+    botaoSens.attachPop(telaSens);
+    botaoSettings.attachPop(telaSettings);
+
+    adicionarSalaExtraAc.attachPop(addSalaExtraAc);
+    adicionarSalaExtraLuz.attachPop(addSalaExtraLuz);
+    adicionarSalaExtraProj.attachPop(addSalaExtraProj);
+    adicionarSalaExtraTv.attachPop(addSalaExtraTv);
+
+    removerSalaExtraLuz.attachPop(removeSalaExtraLuz);
+    removerSalaExtraAc.attachPop(removeSalaExtraAc);
+    removerSalaExtraAc3.attachPop(removeSalaExtraAc);
+    removerSalaExtraProj.attachPop(removeSalaExtraProj);
+    removerSalaExtraTv.attachPop(removeSalaExtraTv);
+    removerSalaExtraTv2.attachPop(removeSalaExtraTv);
+
 
     // Limpa a lista interna de componentes monitorados.
     nexClearListenList();
@@ -129,12 +126,28 @@ void configurarEventosNextion()
     nexListen(botaoBackTv2);
     nexListen(botaoBackTv3);
 
+    nexListen(adicionarSalaExtraAc);
+    nexListen(adicionarSalaExtraLuz);
+    nexListen(adicionarSalaExtraProj);
+    nexListen(adicionarSalaExtraTv);
+
+    nexListen(removerSalaExtraLuz);
+    nexListen(removerSalaExtraAc);
+    nexListen(removerSalaExtraAc3);
+    nexListen(removerSalaExtraProj);
+    nexListen(removerSalaExtraTv);
+    nexListen(removerSalaExtraTv2);
+
     nexListen(botaoLuz);
+    nexListen(botaoAr);
+    nexListen(botaoProj);
+    nexListen(botaoTv);
+    nexListen(botaoSens);
+    nexListen(botaoSettings);
 }
 
 void botaoBack()
 {
-    Serial.println("Botao b0 solto.");
     sendCommand("page inicial");
 }
 
@@ -145,3 +158,86 @@ void telaLuz()
     else
     sendCommand("page luz1");
 }
+
+void telaAr()
+{
+    if(arSalaExtra)
+    sendCommand("page ac2");
+    else
+    sendCommand("page ac1");
+}
+
+void telaProj()
+{
+    if(projSalaExtra)
+    sendCommand("page proj2");
+    else
+    sendCommand("page proj1");
+}
+
+void telaTv()
+{
+    if(tvSalaExtra)
+    sendCommand("page tv2");
+    else
+    sendCommand("page tv1");
+}
+
+void telaSens()
+{
+    sendCommand("page sens");
+}
+
+void telaSettings()
+{
+    sendCommand("page settings");
+}
+
+void addSalaExtraLuz()
+{
+    luzSalaExtra = 1;
+    telaLuz();
+}
+
+void addSalaExtraAc()
+{
+    arSalaExtra = 1;
+    telaAr();
+}
+
+void addSalaExtraProj()
+{
+    projSalaExtra = 1;
+    telaProj();
+}
+
+void addSalaExtraTv()
+{
+    tvSalaExtra = 1;
+    telaTv();
+}
+
+void removeSalaExtraLuz()
+{
+    luzSalaExtra = 0;
+    telaLuz();
+}
+
+void removeSalaExtraAc()
+{
+    arSalaExtra = 0;
+    telaAr();
+}
+
+void removeSalaExtraProj()
+{
+    projSalaExtra = 0;
+    telaProj();
+}
+
+void removeSalaExtraTv()
+{
+    tvSalaExtra = 0;
+    telaTv();
+}
+
