e5214621ae9cc6ebae4ec58e72aeaaeb39bd68ec
GUILHERME AUGUSTO PRINCE <54135432808@info.local>
2026-06-02 17:12:29 -0300

    Adicionando timestamp e serialização AC

--- src/Serialize.cpp
+++ src/Serialize.cpp
@@ -2,18 +2,45 @@
 #include <ArduinoJson.h>
 #include <MqttManager.h>
 #include <DebugManager.h>
+#include <ezTime.h>
+
+
 
 String mensagemTv;
 String mensagemLuz;
+String mensagemProj;
+String mensagemTela;
+String mensagemAC;
 
-void serializarAc()
+
+void iniciaTimestamp()
 {
+    setInterval(3600);
+    waitForSync();
+    tempo.setLocation("America/Sao_Paulo");
+
+}
+
+void serializarAc(int esp, int qualAr, bool estado, int temp, int modo, int vento)
+{
+    JsonDocument doc;
+    doc["ar-condicionado"]["esp"] = esp;
+    doc["ar-condicionado"]["id_ar"] = qualAr;
+    doc["ar-condicionado"]["estado"] = estado;
+    doc["ar-condicionado"]["temperatura"] = temp;
+    doc["ar-condicionado"]["modo"] = modo;
+    doc["ar-condicionado"]["vento"] = vento;
+    doc["timestamp"] = tempo.now();
+    serializeJson(doc, mensagemAC);
+    publicarMensagemNoTopico(TOPICO_AC, mensagemAC.c_str());
+    debugInfo(mensagemAC);
 }
 
 void serializeTv(int comando)
 {
     JsonDocument doc;
     doc["televisao"]["comando"] = comando;
+    doc["timestamp"] = tempo.now();
     serializeJson(doc, mensagemTv);
     publicarMensagemNoTopico(TOPICO_TV, mensagemTv.c_str());
     Serial.printf("\n%s\n", mensagemTv);
@@ -25,17 +52,29 @@ void serializeLampada(int sala, bool frenteAtras, bool estadoLampada)
     if (sala == 9)
     {
         if (frenteAtras)
+        {
             doc["lampadaSala09"]["interruptor1"] = estadoLampada;
+            doc["timestamp"] = tempo.now();
+        }
         else
+        {
             doc["lampadaSala09"]["interruptor2"] = estadoLampada;
+            doc["timestamp"] = tempo.now();
     }
+}
     else if (sala == 10)
     {
         if (frenteAtras)
+        {
             doc["lampadaSala10"]["interruptor3"] = estadoLampada;
+            doc["timestamp"] = tempo.now();
+        }
         else
+        {
             doc["lampadaSala10"]["interruptor4"] = estadoLampada;
+            doc["timestamp"] = tempo.now();
     }
+}
 
     serializeJson(doc, mensagemLuz);
     debugInfo(mensagemLuz);
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,29 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <Adafruit_NeoPixel.h>
+#include <ezTime.h>
 
 #include "WiFiManager.h"
 #include "MqttManager.h"
 #include "DebugManager.h"
 #include "NextionManager.h"
+#include "Serialize.h"
 
 void setup()
 {
   configDebug();
-
+  
+    configurarNextion();
+    configurarTelaInicial();
+    configurarEventosNextion();
+    iniciaTimestamp();
   delay(1000);
+  
+
+
 
   Serial.println();
   Serial.println("Iniciando exemplo com ESP32-S3 e Nextion...");
-
-  configurarNextion();
-  configurarTelaInicial();
-  configurarEventosNextion();
 
   Serial.println("Sistema iniciado.");
 }
@@ -26,4 +31,5 @@ void setup()
 void loop()
 {
   nexLoop();
+  events();
 }
